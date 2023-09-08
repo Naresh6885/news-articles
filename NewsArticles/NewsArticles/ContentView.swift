b88bd243f962b0a5abde6febb092fbaff2a90543
Naresh <naresh@Nareshs-MacBook-Air.local>
2023-09-08 12:21:36 +0530

    project created initial

---
+++ NewsArticles/NewsArticles/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NewsArticles
+//
+//  Created by Naresh on 08/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsArticles/NewsArticles/NewsArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsArticlesApp.swift
+//  NewsArticles
+//
+//  Created by Naresh on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
